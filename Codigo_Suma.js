144f6074fc3d465340c4ee76d533fa000f907b84
JDEspina2010 <josuedaniel.espina@gmail.com>
2026-09-21 09:11:03 -0600

    Pepe el crack

---
+++ Codigo_Suma.js
@@ -0,0 +1,21 @@
+let activo = document.getElementById("Num1");
+
+document.getElementById("Num1").onclick = () => activo = document.getElementById("Num1");
+document.getElementById("Num2").onclick = () => activo = document.getElementById("Num2");
+
+document.querySelectorAll(".numero").forEach(btn => {
+    btn.onclick = () => activo.value += btn.innerText;
+});
+
+document.getElementById("borrar").onclick = () => {
+    document.getElementById("Num1").value = "";
+    document.getElementById("Num2").value = "";
+    document.getElementById("resultado").innerText = "";
+    activo = document.getElementById("Num1"); 
+};
+
+document.getElementById("calcular").onclick = () => {
+    let n1 = Number(document.getElementById("Num1").value);
+    let n2 = Number(document.getElementById("Num2").value);
+    document.getElementById("resultado").innerText = "Resultado: " + (n1 + n2);
+};
