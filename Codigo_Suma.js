8df5e09a20a6dd90b1766c3bc83f2ebd5c98ccbb
JDEspina2010 <josuedaniel.espina@gmail.com>
2026-09-23 11:37:02 -0600

    Aqui mas implementaciones

--- Codigo_Suma.js
+++ Codigo_Suma.js
@@ -11,7 +11,7 @@ document.getElementById("borrar").onclick = () => {
     document.getElementById("Num1").value = "";
     document.getElementById("Num2").value = "";
     document.getElementById("resultado") = "";
-    activo = document.getElementById("Num1"); 
+    activo = document.getElementById("Num1");
 };
 
 document.getElementById("calcular").onclick = () => {
@@ -19,3 +19,34 @@ document.getElementById("calcular").onclick = () => {
     let n2 = Number(document.getElementById("Num2").value);
     document.getElementById("resultado").innerText = "Resultado: " + (n1 + n2);
 };
+//Parte de el carrusel
+const carruselElemento = document.querySelector(".carrusel-items");
+
+let posicionMaximaDesplazamiento = carruselElemento.scrollWidth - carruselElemento.clientWidth;
+let intervaloDesplazamiento = null;
+let pasoDesplazamiento = 1;
+
+const iniciarDesplazamiento = () => {
+    intervaloDesplazamiento = setInterval(function () {
+        carruselElemento.scrollLeft = carruselElemento.scrollLeft + pasoDesplazamiento;
+        if (carruselElemento.scrollLeft === posicionMaximaDesplazamiento) {
+            pasoDesplazamiento = pasoDesplazamiento * -1;
+        } else if (carruselElemento.scrollLeft === 0) {
+            pasoDesplazamiento = pasoDesplazamiento * -1;
+        }
+    }, 10);
+};
+
+const detenerDesplazamiento = () => {
+    clearInterval(intervaloDesplazamiento);
+};
+
+carruselElemento.addEventListener("mouseover", () => {
+    detenerDesplazamiento();
+});
+
+carruselElemento.addEventListener("mouseout", () => {
+    iniciarDesplazamiento();
+});
+
+iniciarDesplazamiento();
